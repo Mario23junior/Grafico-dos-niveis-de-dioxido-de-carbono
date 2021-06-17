c52e12b2003003df43b3523d552216e0d5aa4822
Mario23junior <mariojunior3251@gmail.com>
2021-06-17 09:30:47 -0300

    inserindo panel para renderizar grafico experimental

---
+++ src/main/java/project/nivel_dioxido_carbono/WindowGrafico/JanelaGrafico.java
@@ -0,0 +1,70 @@
+package project.nivel_dioxido_carbono.WindowGrafico;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Panel;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+import project.nivel_dioxido_carbono.CreateGrafico.GerarGrafico;
+import project.nivel_dioxido_carbono.CreateGrafico.Model.NiveisDeDioxidoDeCarbono;
+
+public class JanelaGrafico extends JFrame {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+    
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JanelaGrafico frame = new JanelaGrafico();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	/**
+	 * Create the frame.
+	 */
+	
+
+	public JanelaGrafico() {
+		Panel jpnPanelGrafic = new Panel();
+		getContentPane().add(jpnPanelGrafic, BorderLayout.CENTER);	
+		
+		ArrayList<NiveisDeDioxidoDeCarbono> listDados = new ArrayList<NiveisDeDioxidoDeCarbono>();
+		
+		listDados.add(new NiveisDeDioxidoDeCarbono("São paulo", 2009L, 232));
+		
+		GerarGrafico create = new GerarGrafico();
+		
+		getContentPane().add(create.createGrafico(listDados));
+		
+		pack();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
