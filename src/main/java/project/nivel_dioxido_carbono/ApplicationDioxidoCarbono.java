86a566416ea626879a28eeb2918a50b1a1115327
Mario23junior <mariojunior3251@gmail.com>
2021-06-17 06:31:57 -0300

    iniciando projeto de graficos utilizando Jfreechart

---
+++ src/main/java/project/nivel_dioxido_carbono/ApplicationDioxidoCarbono.java
@@ -0,0 +1,8 @@
+package project.nivel_dioxido_carbono;
+
+
+public class ApplicationDioxidoCarbono {
+    public static void main( String[] args ) {
+        System.out.println( "Projeto graficos utilizando JFreeChart");
+    }
+}
